46b34e93c2719ee9844faf3578c2373d94446828
渡邊丈洋 <take-watanabe@autobacs.com>
2024-07-29 08:45:00 +0900

    Update & Create files.
    EC2インスタンス作成ワークを追加

---
+++ data.tf
@@ -0,0 +1,21 @@
+data "aws_prefix_list" "s3_pl" {
+  name = "com.amazonaws.*.s3"
+}
+
+data "aws_ami" "app" {
+  most_recent = true
+  owners      = ["self", "amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*-x86_64-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
